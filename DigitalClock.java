05ff1d78a1a050e3f016343625af7ee8a17b12af
jsingh10101 <43722216+jsingh10101@users.noreply.github.com>
2018-12-03 04:23:59 +0530

    Update DigitalClock.java

--- DigitalClock.java
+++ DigitalClock.java
@@ -20,6 +20,7 @@ public class DigitalClock extends Applet {
     }
     public void paint(Graphics g){
         long l=System.currentTimeMillis();
+	int wd=getSize().width,ht=getSize().height;
         l/=1000;
         l%=(24*3600);
         int min=((int)l/60)%60+30;
@@ -28,9 +29,17 @@ public class DigitalClock extends Applet {
             hr++;
             min-=60;
         }
-        if(hr>=24)
+	if(hr>=24)
             hr-=24;
-        int sec=(int)l%60;
+	int sec=(int)l%60;
+ 	drawer(br(hr/10),g,1,wd/2+30,ht/4);
+        drawer(br(hr%10),g,2,wd/2+30,ht/4);
+        drawer(null,g,3,wd/2+30,ht/4);
+        drawer(br(min/10),g,4,wd/2+30,ht/4);
+        drawer(br(min%10),g,5,wd/2+30,ht/4);
+        drawer(null,g,6,wd/2+30,ht/4);
+        drawer(br(sec/10),g,7,wd/2+30,ht/4);
+        drawer(br(sec%10),g,8,wd/2+30,ht/4);
         int j=33;
         if(hr>12) {
             hr -= 12;
@@ -38,17 +47,17 @@ public class DigitalClock extends Applet {
         }
         else if(hr==0)
             hr=12;
-        drawer(br(hr/10),g,1);
-        drawer(br(hr%10),g,2);
-        drawer(null,g,3);
-        drawer(br(min/10),g,4);
-        drawer(br(min%10),g,5);
-        drawer(null,g,6);
-        drawer(br(sec/10),g,7);
-        drawer(br(sec%10),g,8);
-        drawer(br(j),g,10);
-        drawer(br(65),g,11);
-        drawer(br(66),g,12);
+        drawer(br(hr/10),g,1,wd/2,3*ht/4);
+        drawer(br(hr%10),g,2,wd/2,3*ht/4);
+        drawer(null,g,3,wd/2,3*ht/4);
+        drawer(br(min/10),g,4,wd/2,3*ht/4);
+        drawer(br(min%10),g,5,wd/2,3*ht/4);
+        drawer(null,g,6,wd/2,3*ht/4);
+        drawer(br(sec/10),g,7,wd/2,3*ht/4);
+        drawer(br(sec%10),g,8,wd/2,3*ht/4);
+        drawer(br(j),g,10,wd/2,3*ht/4);
+        drawer(br(65),g,11,wd/2,3*ht/4);
+        drawer(br(66),g,12,wd/2,3*ht/4);
     }
     private boolean[] br(int n){
         boolean[] b={true,true,true,true,true,true,true};
@@ -83,28 +92,28 @@ public class DigitalClock extends Applet {
         return b;
     }
 
-    private void drawer(boolean[] b,Graphics g,int gap){
-        int y1=140,y2=150,y3=160,x1=120+15*--gap,x2=130+15*--gap;
+    private void drawer(boolean[] b,Graphics g,int gap,int wd,int ht){
+        int y2=ht,y1=y2-10,y3=y2+10,x1=wd-80+15*--gap,x2=x1+10;
         if(b==null){
-            g.fillRect(x1-2,y1+3,3,3);
-            g.fillRect(x1-2,y1+13,3,3);
+            g.fillRect(x1+5,y1+3,3,3);
+            g.fillRect(x1+5,y1+13,3,3);
         }
         else {
             if (!(b[4] || b[3] || b[6])&&b[5]) {
-                x1 -= 10;
-                x2 -= 10;
+                x1 -= 5;
+                x2 -= 5;
             }
             if (b[0])
                 g.drawLine(x1, y1, x2, y1);
-            if (b[5])
+            if (b[1])
                 g.drawLine(x2, y1, x2, y2);
-            if (b[4])
+            if (b[2])
                 g.drawLine(x2, y2, x2, y3);
             if (b[3])
                 g.drawLine(x1, y3, x2, y3);
-            if (b[2])
+            if (b[4])
                 g.drawLine(x1, y2, x1, y3);
-            if (b[1])
+            if (b[5])
                 g.drawLine(x1, y1, x1, y2);
             if (b[6])
                 g.drawLine(x1, y2, x2, y2);
